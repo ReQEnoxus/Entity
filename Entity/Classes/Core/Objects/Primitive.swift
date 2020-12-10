6aedb4df1aa7c67b48fbae316981da86e18efa8d
enoxus <reqenoxus@gmail.com>
2020-12-10 15:51:28 +0300

    added CaseIterable conformance to primitive types and bumped version

--- Entity/Classes/Core/Objects/Primitive.swift
+++ Entity/Classes/Core/Objects/Primitive.swift
@@ -5,7 +5,7 @@
 //  Created by Enoxus on 25.10.2020.
 //
 
-public enum Primitive: Int, Codable {
+public enum Primitive: Int, Codable, CaseIterable {
     case number = 0
     case bool = 1
     case string = 2
